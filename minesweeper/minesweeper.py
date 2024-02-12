f820b13ceb74e3c744bad392247195b53b933a9b
Nathan Franklin <nathanfranklin@live.co.uk>
2024-02-12 22:34:59 +0000

    add flag functionality

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -29,6 +29,7 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 PINK = (255, 192, 203)
 LIGHT_BLUE = (173, 216, 230)
+GREEN = (0, 255, 0)
 
 # images
 class Tiles:
@@ -72,6 +73,10 @@ class Tile_results:
         self.img = pygame.transform.scale(pygame.image.load(os.path.join(PATH, 'img', self.png)), (img_width, img_height))
         # self.is_hovered = False
 
+flag = Tile_results('flag')
+flag_rect = pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT)
+radius = int(TILE_WIDTH/2)
+circle_width = int(TILE_WIDTH/6)
 bomb = Tile_results('ms_bomb')
 zero = Tile_results('zero')
 one = Tile_results('one')
@@ -132,9 +137,14 @@ def main():
     WIN.fill(LIGHT_BLUE)
     for tile in tile_list:
         tile.draw_tile(WIN)
+    # flag_rect = pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT)
+    WIN.blit(flag.img, flag_rect)
+    # pygame.draw.circle(WIN, GREEN, (GRID_WIDTH/2, TOP_SECTION/2), radius, circle_width)
     pygame.display.update()
     add_bombs()
     clicked = False
+    flag_hovered = False
+    flag_clicked = False
     run = True
     while run:
         clock.tick(FPS)
@@ -147,51 +157,82 @@ def main():
             elif event.type == pygame.MOUSEMOTION:
                 # shows which tile is being hovered over
                 for tile in tile_list:
-                    if tile.rect.collidepoint(event.pos) and tile.is_hovered == False and revealed[tile.row][tile.col] == 0: # if you're hovering over it and you weren't before
+                    if tile.rect.collidepoint(event.pos) and tile.is_hovered == False and tile.is_flagged == False and revealed[tile.row][tile.col] == 0: # if you're hovering over it and you weren't before
                         tile.is_hovered = True # it's hovered now
                         tile.draw_tile(WIN) # draw the hovered version
                         pygame.display.update()
-                    elif tile.rect.collidepoint(event.pos) == False and tile.is_hovered == True and revealed[tile.row][tile.col] == 0: # if you're not hovering over the square but you were before
+                    elif tile.rect.collidepoint(event.pos) == False and tile.is_hovered == True and tile.is_flagged == False and revealed[tile.row][tile.col] == 0: # if you're not hovering over the square but you were before
                         tile.is_hovered = False # now it's not hovered
                         tile.draw_tile(WIN) # draw the normal version
                         pygame.display.update()
+                
+                # hovering over the flag at the top
+                if pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) and flag_hovered == False and flag_clicked == False:
+                    flag_hovered = True
+                    WIN.blit(flag.img, flag_rect)
+                    pygame.draw.circle(WIN, PINK, (GRID_WIDTH/2, TOP_SECTION/2), radius, circle_width)
+                    pygame.display.update()
+                elif pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) == False and flag_hovered == True and flag_clicked == False:
+                    flag_hovered = False
+                    WIN.blit(flag.img, flag_rect)
+                    pygame.display.update()
             
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if clicked == False:
                     clicked = True
+                    # clicking on any tile
                     for tile in tile_list:
                         if tile.is_clicked(pygame.mouse.get_pos()):
-                            if grid[tile.row][tile.col] == 8038:
-                                WIN.blit(bomb.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 0:
-                                WIN.blit(zero.img, tile.rect)
-                                reveal_zeros(grid, revealed, tile.row, tile.col) # if it's a zero, this will reveal everything connected until it hits a number
-                            elif grid[tile.row][tile.col] == 1:
-                                WIN.blit(one.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 2:
-                                WIN.blit(two.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 3:
-                                WIN.blit(three.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 4:
-                                WIN.blit(four.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 5:
-                                WIN.blit(five.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 6:
-                                WIN.blit(six.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 7:
-                                WIN.blit(seven.img, tile.rect)
-                            elif grid[tile.row][tile.col] == 8:
-                                WIN.blit(eight.img, tile.rect)
+                            if flag_clicked:
+                                if tile.is_flagged == False:
+                                    tile.is_flagged = True
+                                elif tile.is_flagged == True:
+                                    tile.is_flagged = False
+                                tile.draw_tile(WIN)
+                                pygame.display.update()
+                            elif flag_clicked == False:
+                                if tile.is_flagged == False:
+                                    if grid[tile.row][tile.col] == 8038:
+                                        WIN.blit(bomb.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 0:
+                                        WIN.blit(zero.img, tile.rect)
+                                        reveal_zeros(grid, revealed, tile.row, tile.col) # if it's a zero, this will reveal everything connected until it hits a number
+                                    elif grid[tile.row][tile.col] == 1:
+                                        WIN.blit(one.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 2:
+                                        WIN.blit(two.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 3:
+                                        WIN.blit(three.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 4:
+                                        WIN.blit(four.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 5:
+                                        WIN.blit(five.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 6:
+                                        WIN.blit(six.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 7:
+                                        WIN.blit(seven.img, tile.rect)
+                                    elif grid[tile.row][tile.col] == 8:
+                                        WIN.blit(eight.img, tile.rect)
 
-                            revealed[tile.row][tile.col] = 1 # records that it's been revealed
-                            pygame.display.update()
+                                    revealed[tile.row][tile.col] = 1 # records that it's been revealed
+                                    pygame.display.update()
+
+                    # clicking the flag at the top
+                    if pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) and flag_clicked == False and revealed[tile.row][tile.col] == 0:
+                        flag_clicked = True
+                        WIN.blit(flag.img, flag_rect)
+                        pygame.draw.circle(WIN, GREEN, (GRID_WIDTH/2, TOP_SECTION/2), radius, circle_width)
+                        pygame.display.update()
+                    elif pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) and flag_clicked == True and revealed[tile.row][tile.col] == 0:
+                        flag_clicked = False
+                        WIN.blit(flag.img, flag_rect)
+                        pygame.display.update()
 
             elif event.type == pygame.MOUSEBUTTONUP: # prevents one click doing multiple actions
                 clicked = False
 
 main()
 
-# need to add in flags
 # need to add in a clock element to time it
 # need to add in win logic
 # need to add in loss logic
