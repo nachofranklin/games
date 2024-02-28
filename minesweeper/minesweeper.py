d30d86c05995a9eb9dbf7a6fb8497dce86c8f540
Nathan Franklin <nathanfranklin@live.co.uk>
2024-02-28 23:07:12 +0000

    add win celebration

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -32,6 +32,10 @@ BLUE = (0, 0, 255)
 PINK = (255, 192, 203)
 LIGHT_BLUE = (173, 216, 230)
 GREEN = (0, 255, 0)
+ORANGE = (255, 165, 0)
+
+# sounds
+EXPLOSION_SOUND = pygame.mixer.Sound(os.path.join(PATH, 'sounds', 'explosion.wav'))
 
 # images
 class Tiles:
@@ -88,9 +92,11 @@ six = Tile_results('six')
 seven = Tile_results('seven')
 eight = Tile_results('eight')
 
+timer_text_pos = (WIN_WIDTH/4, TOP_SECTION/2)
 flag_text_pos = (WIN_WIDTH*3/4, TOP_SECTION/2)
 font_size = int(TOP_SECTION)
-# function will only work for showing flags / no of bombs
+
+# function for timer and flag counter / total bombs
 def draw_text(text, position, colour = BLUE, font_size = font_size, screen = WIN):
     font = pygame.font.Font(None, font_size)
     max_text_surface = font.render('100 / 100', True, colour)
@@ -148,6 +154,63 @@ def reveal_zeros(grid, revealed, row, col):
                         WIN.blit(eight.img, pygame.Rect(new_col * TILE_WIDTH, new_row * TILE_HEIGHT + TOP_SECTION, TILE_WIDTH, TILE_HEIGHT))
                     reveal_zeros(grid, revealed, new_row, new_col)
 
+class Text:
+    def __init__(self, x, y, text, text_colour=BLACK, text_size=font_size*2):
+        self.rect = pygame.Rect(x, y, 0, 0)
+        self.text = text
+        self.text_colour = text_colour
+        self.text_size = text_size
+        self.font = pygame.font.Font(None, self.text_size)
+    
+    def blit_text(self, screen):
+        text_surface = self.font.render(self.text, True, self.text_colour)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
+
+YOU_LOSE_ONE = Text(GRID_WIDTH/2, TOP_SECTION + GRID_HEIGHT/2, 'You Lose', BLACK)
+YOU_LOSE_TWO = Text(GRID_WIDTH/2, TOP_SECTION + GRID_HEIGHT/2, 'You Lose', PINK)
+YOU_WIN = Text(GRID_WIDTH/2, TOP_SECTION + GRID_HEIGHT/2, 'You Win!!!', BLACK)
+
+def you_lose(screen):
+    EXPLOSION_SOUND.play()
+    YOU_LOSE_ONE.blit_text(screen)
+    pygame.display.update()
+    pygame.time.delay(1000)
+    YOU_LOSE_TWO.blit_text(screen)
+    pygame.display.update()
+    pygame.time.delay(1000)
+    YOU_LOSE_ONE.blit_text(screen)
+    pygame.display.update()
+    pygame.time.delay(1000)
+    YOU_LOSE_TWO.blit_text(screen)
+    pygame.display.update()
+    pygame.time.delay(1000)
+    YOU_LOSE_ONE.blit_text(screen)
+    pygame.display.update()
+
+def you_win(screen):
+    bomb_list = []
+    for tile in tile_list:
+        if grid[tile.row][tile.col] == 8038:
+            bomb_list.append(tile)
+    random.shuffle(bomb_list)
+    YOU_WIN.blit_text(screen)
+    pygame.display.update()
+    pygame.time.delay(3000)
+    for b in bomb_list:
+        screen.blit(bomb.img, b.rect)
+        YOU_WIN.blit_text(screen)
+        EXPLOSION_SOUND.play()
+        pygame.display.update()
+        pygame.time.delay(750)
+
+def start_timer(start_time):
+    timer_seconds = (pygame.time.get_ticks() - start_time) / 1000
+    minutes, seconds = divmod(timer_seconds, 60)
+    formatted_time = "{:02}:{:02}".format(int(minutes), int(seconds))
+    draw_text(f"{formatted_time}", timer_text_pos)
+    pygame.display.update()
+
 def main():
 
     clock = pygame.time.Clock()
@@ -162,6 +225,9 @@ def main():
     clicked = False
     flag_hovered = False
     flag_clicked = False
+    game_over = False
+    timer_started = False
+    revealed_count = 0
     run = True
     while run:
         clock.tick(FPS)
@@ -172,30 +238,31 @@ def main():
                 exit()
 
             elif event.type == pygame.MOUSEMOTION:
-                # shows which tile is being hovered over
-                for tile in tile_list:
-                    if tile.rect.collidepoint(event.pos) and tile.is_hovered == False and tile.is_flagged == False and revealed[tile.row][tile.col] == 0: # if you're hovering over it and you weren't before
-                        tile.is_hovered = True # it's hovered now
-                        tile.draw_tile(WIN) # draw the hovered version
+                if game_over == False:
+                    # shows which tile is being hovered over
+                    for tile in tile_list:
+                        if tile.rect.collidepoint(event.pos) and tile.is_hovered == False and tile.is_flagged == False and revealed[tile.row][tile.col] == 0: # if you're hovering over it and you weren't before
+                            tile.is_hovered = True # it's hovered now
+                            tile.draw_tile(WIN) # draw the hovered version
+                            pygame.display.update()
+                        elif tile.rect.collidepoint(event.pos) == False and tile.is_hovered == True and tile.is_flagged == False and revealed[tile.row][tile.col] == 0: # if you're not hovering over the square but you were before
+                            tile.is_hovered = False # now it's not hovered
+                            tile.draw_tile(WIN) # draw the normal version
+                            pygame.display.update()
+                    
+                    # hovering over the flag at the top
+                    if pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) and flag_hovered == False and flag_clicked == False:
+                        flag_hovered = True
+                        WIN.blit(flag.img, flag_rect)
+                        pygame.draw.circle(WIN, PINK, (GRID_WIDTH/2, TOP_SECTION/2), radius, circle_width)
                         pygame.display.update()
-                    elif tile.rect.collidepoint(event.pos) == False and tile.is_hovered == True and tile.is_flagged == False and revealed[tile.row][tile.col] == 0: # if you're not hovering over the square but you were before
-                        tile.is_hovered = False # now it's not hovered
-                        tile.draw_tile(WIN) # draw the normal version
+                    elif pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) == False and flag_hovered == True and flag_clicked == False:
+                        flag_hovered = False
+                        WIN.blit(flag.img, flag_rect)
                         pygame.display.update()
-                
-                # hovering over the flag at the top
-                if pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) and flag_hovered == False and flag_clicked == False:
-                    flag_hovered = True
-                    WIN.blit(flag.img, flag_rect)
-                    pygame.draw.circle(WIN, PINK, (GRID_WIDTH/2, TOP_SECTION/2), radius, circle_width)
-                    pygame.display.update()
-                elif pygame.Rect(GRID_WIDTH/2 - TILE_WIDTH/2, TOP_SECTION/2 - TILE_HEIGHT/2, TILE_WIDTH, TILE_HEIGHT).collidepoint(event.pos) == False and flag_hovered == True and flag_clicked == False:
-                    flag_hovered = False
-                    WIN.blit(flag.img, flag_rect)
-                    pygame.display.update()
             
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if clicked == False:
+                if clicked == False and game_over == False:
                     clicked = True
                     # clicking on any tile
                     for tile in tile_list:
@@ -211,9 +278,14 @@ def main():
                                 draw_text(f'{flag_counter} / {no_of_bombs}', flag_text_pos)
                                 pygame.display.update()
                             elif flag_clicked == False:
+                                if timer_started == False:
+                                    timer_started = True
+                                    start_time = pygame.time.get_ticks()
                                 if tile.is_flagged == False:
                                     if grid[tile.row][tile.col] == 8038:
                                         WIN.blit(bomb.img, tile.rect)
+                                        you_lose(WIN)
+                                        game_over = True
                                     elif grid[tile.row][tile.col] == 0:
                                         WIN.blit(zero.img, tile.rect)
                                         reveal_zeros(grid, revealed, tile.row, tile.col) # if it's a zero, this will reveal everything connected until it hits a number
@@ -251,8 +323,24 @@ def main():
             elif event.type == pygame.MOUSEBUTTONUP: # prevents one click doing multiple actions
                 clicked = False
 
+        # checking if you win
+        revealed_count = 0
+        for tile in tile_list:
+            if revealed[tile.row][tile.col] == 1:
+                revealed_count += 1
+        if revealed_count == GRID_ROWS * GRID_COLS - no_of_bombs and game_over == False:
+            you_win(WIN)
+            game_over = True
+
+        # starting/stopping the timer
+        if timer_started and game_over == False:
+            start_timer(start_time)
+
 main()
 
-# need to add in a clock element to time it
-# need to add in win logic
-# need to add in loss logic
+# best scores (10x10 with 17 bombs)
+
+# nathan - 1.17, 1.28, 1.30
+# georgie - 0.00, 0.18
+# ellie - 4.52
+# almantas - 1.30
