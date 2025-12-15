78418e5e3dd17df811f2c1d0c719c82cd15f00d0
nachofranklin <79118590+nachofranklin@users.noreply.github.com>
2025-12-15 16:20:55 +0000

    fix map room click bug
    when selecting a room, if you clicked quickly enough on another room it would try to load both and crash

--- godot/sts-tutorial/scripts/map/map_room.gd
+++ godot/sts-tutorial/scripts/map/map_room.gd
@@ -1,6 +1,7 @@
 extends Area2D
 class_name MapRoom
 
+signal clicked(room: Room)
 signal selected(room: Room)
 
 const ICONS: Dictionary = {
@@ -46,6 +47,7 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 		return
 	
 	room.selected = true
+	clicked.emit(room)
 	animation_player.play("select")
 
 
