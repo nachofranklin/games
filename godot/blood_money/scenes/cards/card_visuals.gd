5871f31f12bf06124564b1a21542280d9c044879
nachofranklin <79118590+nachofranklin@users.noreply.github.com>
2026-02-10 16:35:32 +0000

    update card_visuals
    had an issue with the icon borders and keeping the art restricted to the given area. All fixed

--- godot/blood_money/scenes/cards/card_visuals.gd
+++ godot/blood_money/scenes/cards/card_visuals.gd
@@ -7,13 +7,14 @@ class_name CardVisuals
 @onready var card_description: RichTextLabel = %CardDescription
 @onready var exhausts_label: Label = %ExhaustsLabel
 @onready var card_name_label: Label = %CardNameLabel
+@onready var icon_border: PanelContainer = %IconBorder
 @onready var energy_background: Panel = %EnergyBackground
 @onready var energy_label: Label = %EnergyLabel
 
 var cards_theme: Theme = preload('res://art/theme/cards_theme.tres')
 var card_colour: Dictionary = {'CURSE': Color.BLACK, 'STATUS': Color.DIM_GRAY, 'SHOP': Color.WHITE, 'NACHO': Color.DEEP_SKY_BLUE} # add more characters here
 var rarity_colour: Dictionary = {'COMMON': Color.GRAY, 'UNCOMMON': Color.LIGHT_SKY_BLUE, 'RARE': Color.GOLD}
-
+var border_width: int = 3
 
 func update_visuals(card: Card) -> void:
 	_update_styleboxes(card)
@@ -31,6 +32,7 @@ func _update_styleboxes(card: Card) -> void:
 	var bg_pc_sb: StyleBoxFlat = background_panel_container.get_theme_stylebox('panel').duplicate()
 	var mana_p_sb: StyleBoxFlat = energy_background.get_theme_stylebox('panel').duplicate()
 	var icon_pc_sb: StyleBoxFlat
+	var icon_b_sb: StyleBoxFlat
 	
 	match card.card_type: # if curse or status then keep the skill one?
 		Card.CardType.ATTACK:
@@ -43,6 +45,8 @@ func _update_styleboxes(card: Card) -> void:
 			icon_pc_sb = cards_theme.get_stylebox('skill_pc', 'PanelContainer').duplicate()
 		Card.CardType.STATUS:
 			icon_pc_sb = cards_theme.get_stylebox('skill_pc', 'PanelContainer').duplicate()
+	
+	icon_b_sb = icon_pc_sb.duplicate()
 	
 	# edit the StyleBoxFlat
 	if card.card_type == Card.CardType.CURSE:
@@ -61,15 +65,20 @@ func _update_styleboxes(card: Card) -> void:
 	bg_pc_sb.border_color = background_colour
 	mana_p_sb.bg_color = background_colour
 	
+	icon_b_sb.draw_center = false
+	icon_b_sb.set_border_width_all(border_width)
+	icon_b_sb.set_expand_margin_all(border_width - 1)
+	
 	match card.rarity:
 		Card.Rarity.COMMON:
-			icon_pc_sb.border_color = rarity_colour['COMMON']
+			icon_b_sb.border_color = rarity_colour['COMMON']
 		Card.Rarity.UNCOMMON:
-			icon_pc_sb.border_color = rarity_colour['UNCOMMON']
+			icon_b_sb.border_color = rarity_colour['UNCOMMON']
 		Card.Rarity.RARE:
-			icon_pc_sb.border_color = rarity_colour['RARE']
+			icon_b_sb.border_color = rarity_colour['RARE']
 	
 	# use that StyleBoxFlat
 	background_panel_container.add_theme_stylebox_override('panel', bg_pc_sb)
 	energy_background.add_theme_stylebox_override('panel', mana_p_sb)
 	icon_panel_container.add_theme_stylebox_override('panel', icon_pc_sb)
+	icon_border.add_theme_stylebox_override('panel', icon_b_sb)
